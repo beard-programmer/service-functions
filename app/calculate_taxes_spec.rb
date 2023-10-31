ff995d79bb37e2b851d9ad93cb95494565ce495b
victorshinkevich <supostatekx@gmail.com>
2023-10-31 04:57:11 +0300

    failing test

--- app/calculate_taxes_spec.rb
+++ app/calculate_taxes_spec.rb
@@ -14,6 +14,7 @@ RSpec.describe ServiceFunctions::CalculateTaxes, type: :service_function do
       end
 
       it 'then should calculate taxes and return CalculatedLineItem' do
+        expect(1).to eq(2)
         expect(when_calculating_taxes).to be_instance_of(ServiceFunctions::CalculatedLineItem)
       end
     end
